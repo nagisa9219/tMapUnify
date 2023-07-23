df814eef982506171f157ebaa6f60e3309859726
桐生渚 <nagisadesu87@Nagisa-the-Macbook-Air.local>
2023-07-23 22:24:28 +0800

    small update 2

--- api/weather_api.py
+++ api/weather_api.py
@@ -9,13 +9,15 @@ def weather(lad: float, lon: float):
             lad, lon (float): 地點的經緯度座標
     
     """
-    url="https://api.openweathermap.org/data/2.5/weather?"+lad+"&"+lon+"&appid=d11faca77d78a15c85b9a1f1ea09dcd2"
+    url="https://api.openweathermap.org/data/2.5/weather?lat="+lad+"&lon="+lon+"&appid=d11faca77d78a15c85b9a1f1ea09dcd2"
+    date={}
     resp=requests.get(url).json()
 
     with open ("weather.json","w") as f:
         json.dump(resp,f)
 
     with open("weather.json") as read:
-        data=json.load(read)
-
-    return data
+        data = json.load(read)
+    main_data=data["weather"][0]["description"]
+    
+    return main_data
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultPhoto
-import api.weather_api
-
+import api.weather_api #use weather.weather(address) to get the weather
+from telegram import Update
+from telegram.ext import CallbackContext, MessageHandler, Filters 
 
 API_KEY = '6057511045:AAGut6QZzRU64WDYZmT4bvBk47Mb9srVJD8'
 bot = telebot.TeleBot(API_KEY)
 
+#retart
 backQuery = InlineKeyboardButton("New Option", callback_data="New Option")
 deleteQuery = InlineKeyboardButton("Delete Message", callback_data="Delete")
 renderingMenu = InlineKeyboardMarkup([[
@@ -17,6 +19,10 @@ renderingMenu = InlineKeyboardMarkup([[
   InlineKeyboardButton("taxi", callback_data="taxi"),
 ], [backQuery, deleteQuery]])
 
+def location(update: Update, context: CallbackContext): 
+    current_pos = (update.message.location.latitude, update.message.location.longitude) 
+    print(current_pos) 
+@bot.add_handler(MessageHandler(Filters.location, location))
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -39,10 +45,7 @@ def Delete(call):
 
 @bot.callback_query_handler(func=lambda cb: cb.data == "walking")
 def walking(call):
-  reply_markup = InlineKeyboardMarkup([[
-    backQuery,
-    deleteQuery,
-  ]])
+  reply_markup = InlineKeyboardMarkup([[backQuery,deleteQuery,]])
   bot.reply_to(call.message, 'ok', reply_markup=reply_markup)
   bot.answer_callback_query(call.id, " ")
 
@@ -50,98 +53,35 @@ def walking(call):
 @bot.callback_query_handler(func=lambda cb: cb.data == 'biking')
 def biking(call):
 
-  reply_markup = InlineKeyboardMarkup([[
-    InlineKeyboardButton("North", callback_data="North"),
-    InlineKeyboardButton("South", callback_data="South"),
-    InlineKeyboardButton("East", callback_data="East"),
-    InlineKeyboardButton("Mid", callback_data="Mid"),
-    ],[backQuery,deleteQuery]])
+  reply_markup = InlineKeyboardMarkup([[backQuery,deleteQuery,]])
   bot.reply_to(call.message, 'Enter an option.', reply_markup=reply_markup)
   bot.answer_callback_query(call.id, " ")
 
-  @bot.callback_query_handler(func=lambda cb: cb.data == "North")
-  def North(call):
-    reply_markup = InlineKeyboardMarkup([[
-      InlineKeyboardButton("台北", callback_data="1"),
-      InlineKeyboardButton("基隆", callback_data="2"),
-      InlineKeyboardButton("新北", callback_data="3"),
-      InlineKeyboardButton("桃園", callback_data="4"),
-      InlineKeyboardButton("新竹", callback_data="5"),
-      InlineKeyboardButton("宜蘭", callback_data="6"),
-      ],[backQuery,deleteQuery]])
-    bot.reply_to(call.message, 'ok', reply_markup=reply_markup)
-    bot.answer_callback_query(call.id, " ")
-
-  @bot.callback_query_handler(func=lambda cb: cb.data == "South")
-  def South(call):
-    reply_markup = InlineKeyboardMarkup([[
-      InlineKeyboardButton("嘉義", callback_data="7"),
-      InlineKeyboardButton("台南", callback_data="8"),
-      InlineKeyboardButton("高雄", callback_data="9"),
-      InlineKeyboardButton("屏東", callback_data="10"),
-      ],[backQuery,deleteQuery]])
-    bot.reply_to(call.message, 'ok', reply_markup=reply_markup)
-    bot.answer_callback_query(call.id, " ")
-
-  @bot.callback_query_handler(func=lambda cb: cb.data == "East")
-  def East(call):
-    reply_markup = InlineKeyboardMarkup([[
-      InlineKeyboardButton("花蓮", callback_data="11"),
-      InlineKeyboardButton("台東", callback_data='12'),
-      ],[backQuery,deleteQuery]])
-    bot.reply_to(call.message, 'ok', reply_markup=reply_markup)
-    bot.answer_callback_query(call.id, " ")
-
-  @bot.callback_query_handler(func=lambda cb: cb.data == "Mid")
-  def Mid(call):
-    reply_markup = InlineKeyboardMarkup([[
-      InlineKeyboardButton("苗栗", callback_data="13"),
-      InlineKeyboardButton("台中", callback_data="14"),
-      InlineKeyboardButton("彰化", callback_data="15"),
-      InlineKeyboardButton("雲林", callback_data="16"),
-      InlineKeyboardButton("南投", callback_data="17"),],[backQuery,deleteQuery]])
-    bot.reply_to(call.message, 'ok', reply_markup=reply_markup)
-    bot.answer_callback_query(call.id, " ")
-
-
 @bot.callback_query_handler(func=lambda cb: cb.data == "driving")
 def driving(call):
-  reply_markup = InlineKeyboardMarkup([[
-    backQuery,
-    deleteQuery,
-  ]])
+  reply_markup = InlineKeyboardMarkup([[backQuery,deleteQuery,]])
   bot.reply_to(call.message, 'ok', reply_markup=reply_markup)
   bot.answer_callback_query(call.id, " ")
 
 
 @bot.callback_query_handler(func=lambda cb: cb.data == "scooter")
 def scooter(call):
-  reply_markup = InlineKeyboardMarkup([[
-    backQuery,
-    deleteQuery,
-  ]])
+  reply_markup = InlineKeyboardMarkup([[backQuery,deleteQuery,]])
   bot.reply_to(call.message, 'ok', reply_markup=reply_markup)
-  bot.answer_callback_query(call.id, " ")
+  bot.answer_callback_query(call.id, "")
 
 
 @bot.callback_query_handler(func=lambda cb: cb.data == "mass")
 def mass(call):
-  reply_markup = InlineKeyboardMarkup([[
-    backQuery,
-    deleteQuery,
-  ]])
+  reply_markup = InlineKeyboardMarkup([[backQuery,deleteQuery,]])
   bot.reply_to(call.message, 'ok', reply_markup=reply_markup)
   bot.answer_callback_query(call.id, " ")
 
- 
+
 @bot.callback_query_handler(func=lambda cb: cb.data == "taxi")
 def taxi(call):
-  reply_markup = InlineKeyboardMarkup([[
-    backQuery,
-    deleteQuery,
-  ]])
+  reply_markup = InlineKeyboardMarkup([[backQuery,deleteQuery,]])
   bot.reply_to(call.message, 'ok', reply_markup=reply_markup)
   bot.answer_callback_query(call.id, " ")
-
 
 bot.polling()
