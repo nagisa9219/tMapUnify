52ea0926c27f093a25229ed7af99a09167106aeb
桐生渚 <nagisadesu87@Nagisa-the-Macbook-Air.local>
2023-07-23 20:19:14 +0800

    undated maps_api.py

--- api/maps_api.py
+++ api/maps_api.py
@@ -12,7 +12,9 @@ def address_to_coord(address: str,api_key: str, cityinfo: bool=False) -> list[fl
             cityinfo (bool): 顯示該地址位於的縣市英文名稱(概略)，可略
 
         Returns:
-            lat, lot (float, float): 地址轉換過後的經緯度座標
+            lat, lot (float, float): 地址轉換過後的經緯度座標(cityinfo == False時)
+            [cityname (str)]"
+            [result_text (str): 若查無地址或其他錯誤，則僅回傳此項]
     """
 
     url = "https://api.geoapify.com/v1/geocode/search?text=" + address + "&apiKey=" + api_key
@@ -20,14 +22,14 @@ def address_to_coord(address: str,api_key: str, cityinfo: bool=False) -> list[fl
     headers["Accept"] = "application/json"
 
     resp = requests.get(url, headers=headers).json()
-    coord = resp["features"][0]["geometry"]["coordinates"]
-    
-    if cityinfo == True:
+    if resp["features"] == []:
+        return "Bad Request: Address not found"
+    elif cityinfo == False:
+        coord = resp["features"][0]["geometry"]["coordinates"]
+        return coord
+    elif cityinfo == True:
         city = resp["features"][0]["properties"]["city"]
         return city
-
-    return coord
-
 
 def distance_calc(coord1: list[float], coord2: list[float], mode: str, api_key: str) :
     """
---
+++ test.py
@@ -0,0 +1,36 @@
+# from api.maps_api import address_to_coord
+import requests
+from requests.structures import CaseInsensitiveDict
+
+
+def address_to_coord(address: str,api_key: str, cityinfo: bool=False) -> list[float]:
+    """
+        此function透過Geogapify Geocode API將地址轉換為經緯度座標
+        
+        Args:
+            address (str): 欲轉換的地址
+            api_key (str): API的key
+            cityinfo (bool): 顯示該地址位於的縣市英文名稱(概略)，可略
+
+        Returns:
+            lat, lot (float, float): 地址轉換過後的經緯度座標
+            [result_text (str): 若查無地址或其他錯誤，則僅回傳此項]
+    """
+
+    url = "https://api.geoapify.com/v1/geocode/search?text=" + address + "&apiKey=" + api_key
+    headers = CaseInsensitiveDict()
+    headers["Accept"] = "application/json"
+
+    resp = requests.get(url, headers=headers).json()
+    if resp["features"] == []:
+        return "Bad Request: Address not found"
+    elif cityinfo == False:
+        coord = resp["features"][0]["geometry"]["coordinates"]
+        return coord
+    elif cityinfo == True:
+        city = resp["features"][0]["properties"]["city"]
+        return city
+    
+    
+
+print(address_to_coord("陽明交通大學", "f0305e991db040688c28b59ac6d9fbd5"))
