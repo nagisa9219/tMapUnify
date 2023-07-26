3c55c527520aeff008607ba146e3cf1572889263
nagisadesu87 <nagisadesu87@gmail.com>
2023-07-26 18:51:20 +0800

    Delete api_token.py
    ==

--- api/api_token.py
+++
@@ -1,9 +0,0 @@
-class apitoken():
-    def __init__(self):
-        """
-            儲存本專案所使用到的API Tokens以供其他程式匯入使用
-        """
-        self.telebot = "6352815505:AAFH1z2PUWMCYD-SntAWW5lC9xdVdcGfKRs"
-        self.openweather = "d11faca77d78a15c85b9a1f1ea09dcd2"
-        self.geoaphify = "f0305e991db040688c28b59ac6d9fbd5"
-        self.googlemaps = "AIzaSyB8jGb3zHAnT_47p-c-5avxJ4KieHEs7-c"
