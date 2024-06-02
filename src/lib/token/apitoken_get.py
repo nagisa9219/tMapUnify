355c5c709479e9f8cf74e26b557e6881c762e363
Nagisa <nagisadesu87@gmail.com>
2024-06-03 02:43:59 +0800

    移除測試用程式碼

--- src/lib/token/apitoken_get.py
+++ src/lib/token/apitoken_get.py
@@ -5,10 +5,6 @@ current_path = os.path.join(os.path.dirname(__file__))
 config_path = os.path.abspath(current_path+os.path.sep+"../.."+os.path.sep+"config/config.ini")
 config = configparser.ConfigParser()
 config.read(config_path)
-
-breakpoint
-# print(config["token"])
-
 class token:
     def teleBotToken():
         return config["token"]["teleBotToken"]
@@ -24,6 +20,3 @@ class token:
 
     def openWeatherToken():
         return config["token"]["openWeatherToken"]
-
-# /Users/nagisadesu87/Documents/Visual Studio Code/tMapUnify/src/config/config.ini
-# '/Users/nagisadesu87/Documents/Visual Studio Code/tMapUnify/src/lib/token/src/config/config.ini'
